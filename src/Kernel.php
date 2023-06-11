082634febac689583e8d385b360fa0b6db3fe727
kiwiFella <bakerboy.nz@gmail.com>
2023-06-11 13:49:17 +1200

    hide new secrests - plus change exposed keys

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,11 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+// @BB locad symfony's dotenv package to keep secrets safe
+use Symfony\Component\Dotenv\Dotenv;
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
