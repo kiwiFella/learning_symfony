a98c330bf218ec359a82db015985052895118d6e
kiwiFella <bakerboy.nz@gmail.com>
2023-06-11 13:51:18 +1200

    dont need dotnet just yet

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 // @BB locad symfony's dotenv package to keep secrets safe
-use Symfony\Component\Dotenv\Dotenv;
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
+// use Symfony\Component\Dotenv\Dotenv;
+// $dotenv = new Dotenv();
+// $dotenv->load(__DIR__.'/.env');
 
 class Kernel extends BaseKernel
 {
